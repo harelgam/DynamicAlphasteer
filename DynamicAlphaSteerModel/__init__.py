56e6a4e83fdeb54ac78c8e924cd7f6653c99831c
Harel Gil <148385097+harelgam@users.noreply.github.com>
2026-01-08 18:24:29 +0200

    Delete DynamicAlphaSteerModel directory

--- DynamicAlphaSteerModel/__init__.py
+++
@@ -1,8 +0,0 @@
-from .DynamicAlphaLlama import DynamicAlphaLlamaForCausalLM
-from .GatingNetwork import GatingNetwork, train_gating_network
-
-__all__ = [
-    'DynamicAlphaLlamaForCausalLM',
-    'GatingNetwork',
-    'train_gating_network'
-]
